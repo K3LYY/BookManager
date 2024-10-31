09bec9d8595e9cf944e39a54319202d203d09ea3
K3LYY <maliszek.j@gmail.com>
2024-10-31 14:28:57 +0100

    Add saving and reading from txt file functionality

--- BookManager/Saving.cs
+++ BookManager/Saving.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 namespace BookManager
@@ -14,15 +15,18 @@ namespace BookManager
         string tempBook = "";
         public void save(List<Book> books)
         {
-            FileStream stream = new FileStream("E:\\GitProjects\\BookManager\\BooksDB.txt", FileMode.OpenOrCreate);
-            using (StreamWriter write = new StreamWriter(stream, Encoding.UTF8))
+            FileStream stream = new FileStream("E:\\BookManagerFinal\\BookManager\\BooksDB.txt", FileMode.OpenOrCreate);
+            using (StreamWriter write = new StreamWriter(stream))
             {
-                for (int i = 0; i < books.Count; i++)
+                for (int i = 0; i <= books.Count-1; i++)
                 {
-                    tempBook += books[i].Id + "|" + books[i].title + "|" + books[i].author + "|" + books[i].year + "|" + books[i].categoryText;
+                    tempBook += books[i].Id + "|" + books[i].title + "|" + books[i].author + "|" + books[i].year + "|" + books[i].categoryNumber;
+                    Console.WriteLine(books[i].Id + "|" + books[i].title + "|" + books[i].author + "|" + books[i].year + "|" + books[i].categoryText);
                     write.WriteLine(tempBook);                    
                     tempBook = "";
                 }
+                write.Close();
+                stream.Close();
             }
 
         }        
